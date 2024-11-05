cd81a079bfd70f1e0e1013a03ab8989800e19374
chldltkrz <chldltkrz@gmail.com>
2024-11-05 18:14:04 +0900

    added Assets and feature to read data from file
    Signed-off-by: chldltkrz <chldltkrz@gmail.com>

--- Classes/Game.dart
+++ Classes/Game.dart
@@ -6,10 +6,14 @@ class Game {
   late List<Monster> monsters;
   late int defeatedMonsters;
 
+  Game(Character c, List<Monster> m) {
+    this.character = c;
+    this.monsters = m;
+    this.defeatedMonsters = 0;
+  }
   void startGame() {}
   void battle() {}
   Monster getRandomMonster() {
     return Monster("Goblin", 100, 10);
   }
-  
 }
--- main.dart
+++ main.dart
@@ -1,3 +1,59 @@
+import 'dart:io';
+
+import 'Classes/Character.dart';
+import 'Classes/Game.dart';
+import 'Classes/Monster.dart';
+
 void main() {
-  
+  print("Enter Character Name: ");
+  // Character c = loadCharacterStats();
+  // List<Monster> m = loadMonsters();
+  // c.showStatus();
+  // m.forEach((monster) => monster.showStatus());
+  Game g = Game(loadCharacterStats(), loadMonsters());
+  g.startGame();
+}
+
+Character loadCharacterStats() {
+  try {
+    final file = File('Assets/character.txt');
+    final contents = file.readAsStringSync();
+    final stats = contents.split(',');
+    if (stats.length != 3) throw FormatException('Invalid character data');
+
+    int health = int.parse(stats[0]);
+    int attack = int.parse(stats[1]);
+    int defense = int.parse(stats[2]);
+
+    String name = stdin.readLineSync()!;
+    RegExp(r'^[a-zA-Z가-힣]+$').allMatches(name).isEmpty
+        ? {print("Invalid Name, Exit Game."), exit(1)}
+        : "";
+    return Character(name, health, attack, defense);
+  } catch (e) {
+    print('캐릭터 데이터를 불러오는 데 실패했습니다: $e');
+    exit(1);
+  }
+}
+
+List<Monster> loadMonsters() {
+  List<Monster> monsters = [];
+  try {
+    final file = File('Assets/monsters.txt');
+    for (final line in file.readAsStringSync().split('\n')) {
+      final stats = line.split(',');
+      if (stats.length != 3) throw FormatException('Invalid monster data');
+
+      String name = stats[0].toString();
+      int health = int.parse(stats[1]);
+      int attack = int.parse(stats[2]);
+
+      monsters.add(Monster(name, health, attack));
+      monsters.toString();
+    }
+    return monsters;
+  } catch (e) {
+    print('몬스터 데이터를 불러오는 데 실패했습니다: $e');
+    exit(1);
+  }
 }
